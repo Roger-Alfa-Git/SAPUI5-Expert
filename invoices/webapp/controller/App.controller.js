ddcbec325c0bb7b98226142dc46c15b8ef33ac95
Roger-Alfa-Git <rogervalderrabano@alfasolucionesti.com>
2024-11-28 17:04:37 +0000

    05 ESTRUCTURA PROYECTOS SAPUI5

---
+++ invoices/webapp/controller/App.controller.js
@@ -0,0 +1,30 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+],
+    /**
+     * 
+     * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     * @param {typeof sap.m.MessageToast} MessageToast 
+     * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel 
+     */
+    function (Controller, MessageToast, Models, ResourceModel) {
+        "use strict";
+
+        return Controller.extend("alfagroup.invoices.controller.App", {
+
+            onInit: function () {
+
+            },
+
+            onShowHello: function () {
+                //readtext from i18n model
+                var oBundle = this.getView().getModel("i18n").getResourceBundle();
+                // read property from data model
+                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+                var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+                MessageToast.show(sMsg);
+
+            }
+        });
+    });
